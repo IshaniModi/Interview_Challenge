d81c01e63febc3917f4ee89a9e77d0ce71b74df0
IshaniModi <44411956+IshaniModi@users.noreply.github.com>
2018-10-24 02:59:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -30,4 +30,4 @@ require('./api/routes/Transactionroute.js'); //importing route
 app.listen(port);
 
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('RESTful API server started on: ' + port);
